39477495ab952696f53ab849e1c659bebef863c5
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-26 21:49:03 -0700

    added routing that loads components

--- building-components-angular-1-5/1-building-components-angular-1-5-m1-exercise-files/before/ps-movies/module.js
+++ building-components-angular-1-5/1-building-components-angular-1-5-m1-exercise-files/before/ps-movies/module.js
@@ -1,6 +1,21 @@
 (function() {
     "use strict";
 
-    angular.module("psMovies", []);
+    // tell angular I have a dependency on the route module
+    var module = angular.module("psMovies", ["ngRoute"]);
+
+    module.config(function($routeProvider) {
+        $routeProvider
+            // use a template to load a component (movie-list)
+            .when("/list", { template: "<movie-list></movie-list>" })
+            .when("/about", { template: "<app-about></app-about>" })
+            .otherwise({ redirectTo: "/list" });
+    });
+
+    // appAbout component
+    module.component("appAbout", {
+        // in-line template
+        template: "This is the about page"
+    });
 
 }());
