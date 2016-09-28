1d4627f11e07cac905dbc090d5ae0ebc02cfca0f
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-27 21:48:13 -0700

    routing with component router

--- building-components-angular-1-5/1-building-components-angular-1-5-m1-exercise-files/before/ps-movies/module.js
+++ building-components-angular-1-5/1-building-components-angular-1-5-m1-exercise-files/before/ps-movies/module.js
@@ -2,15 +2,22 @@
     "use strict";
 
     // tell angular I have a dependency on the route module
-    var module = angular.module("psMovies", ["ngRoute"]);
+    var module = angular.module("psMovies", ["ngComponentRouter"]);
 
-    module.config(function($routeProvider) {
-        $routeProvider
-            // use a template to load a component (movie-list)
-            .when("/list", { template: "<movie-list></movie-list>" })
-            .when("/about", { template: "<app-about></app-about>" })
-            .otherwise({ redirectTo: "/list" });
-    });
+    // inject service $routerRootComponent. component name: movieApp
+    // now component router knows top level routing and navigation rules
+    module.value("$routerRootComponent", "movieApp");
+
+    // original router *, load to templates instead of components
+    // new router loads directly to components
+    // module.config(function($routeProvider) {
+    //     $routeProvider
+    //         // use a template to load a component (movie-list)
+    //         .when("/list", { template: "<movie-list></movie-list>" })
+    //         .when("/about", { template: "<app-about></app-about>" })
+    //         .otherwise({ redirectTo: "/list" });
+    // });
+
 
     // appAbout component
     module.component("appAbout", {
