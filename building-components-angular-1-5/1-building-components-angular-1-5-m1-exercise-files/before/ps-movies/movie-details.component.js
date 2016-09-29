c6d02941b80e6bf138432262f454d920f01fa93d
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-28 21:42:30 -0700

    added component router lifecycle hook $routerOnActivate

--- building-components-angular-1-5/1-building-components-angular-1-5-m1-exercise-files/before/ps-movies/movie-details.component.js
+++ building-components-angular-1-5/1-building-components-angular-1-5-m1-exercise-files/before/ps-movies/movie-details.component.js
@@ -5,7 +5,17 @@
 
     // movieDetails has id in endpoint
     module.component("movieDetails", {
-        templateUrl: "./ps-movies/movie-details.component.html"
+        templateUrl: "./ps-movies/movie-details.component.html",
+        controllerAs: "model",
+        controller: function() {
+            var vm = this;
+
+            // Component router lifecycle hooks
+            vm.$routerOnActivate = function(next) {
+                console.log(next);
+                vm.id = next.params.id;
+            };
+        }
     });
 
 }());
